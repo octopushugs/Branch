bab76d2797cba2e0f644c972dc2e8ca55a262cfe
Mike <type.foster@gmail.com>
2014-02-08 11:56:41 -0800

    Remade students_controller for API.

---
+++ Web/branch/app/controllers/api/v1/student_controller.rb~~
@@ -0,0 +1,23 @@
+module Api
+	module V1
+		class StudentController < ApplicationController
+			
+			def index
+
+			end
+
+			def show
+
+			end
+
+			def sign_up
+
+			end
+		
+			private
+				def student_params
+				#	params.require()
+			
+		end
+	end
+end
---
+++ Web/branch/app/controllers/api/v1/students_controller.rb
@@ -0,0 +1,33 @@
+module Api
+	module V1
+		class StudentController < ApplicationController
+			
+			def index
+
+			end
+
+			def show
+
+			end
+
+			def create
+				student = Student.new
+				student.username = params[:username]
+				student.password = params[:password]
+				student.email = params[:email]
+				student.dob = params[:dob]
+				student.city = params[:city]
+				student.phone = params[:phone]
+				student.full_name = params[:full_name]
+	
+				student.save
+
+			end
+		
+			private
+				def student_params
+					params.require(:student).permit(:password, :email, :dob, :city, :phone, :full_name)
+			
+		end
+	end
+end
