a6ef96109a94cd25bdc4c05743ca988688cbddac
arsood <arsood@gmail.com>
2014-02-08 15:27:44 -0800

    Further work on APP

--- Mobile/branch/www/js/branch.js
+++ Mobile/branch/www/js/branch.js
@@ -22,3 +22,61 @@ $(document).on("pageshow", "#kid-signup2", function() {
 		});
 	}
 });
+
+/*Signup Process*/
+
+$("#signup-button1").click(function() {
+	if (
+		$("#signup-fullname").val() == "" ||
+		$("#signup-username").val() == "" ||
+		$("#signup-password1").val() == "" ||
+		$("#signup-password2").val() == ""
+	) {
+		return false;
+	} else {
+		$.mobile.changePage("#kid-signup2", { transition: "fade" } );
+	}
+});
+		
+$("#signup-button2").click(function() {
+	if (
+		$("#signup-zipcode").val() == "" ||
+		$("#signup-phone").val() == "" ||
+		$("#signup-email").val() == "" ||
+		$("#signup-bday-month").val() == "" ||
+		$("#signup-bday-day").val() == "" ||
+		$("#signup-bday-year").val() == ""
+	) {
+		alert("Please enter all information");
+		return false;
+	} else {
+		$.post("http://ec2-54-200-118-175.us-west-2.compute.amazonaws.com:3000/api/v1/students", {
+			full_name:$("#signup-fullname").val(),
+			username:$("#signup-username").val(),
+			password:$("#signup-password1").val(),
+			zipcode:$("#signup-zipcode").val(),
+			phone:$("#signup-phone").val(),
+			email:$("#signup-email").val(),
+			dob:$("#signup-bday-month").val() + "-" + $("#signup-bday-day").val() + "-" + $("#signup-bday-year").val()
+		}, function(data) {
+			var ajaxData = $.trim(data);
+		});
+	}
+});
+
+//Specific group page buttons
+
+$("#specific-group-upcoming-button").click(function() {
+	$("#specific-group-about-container").hide();
+	$("#specific-group-event-container").fadeIn();
+});
+
+$("#specific-group-past-button").click(function() {
+	$("#specific-group-about-container").hide();
+	$("#specific-group-event-container").fadeIn();
+});
+
+$("#specific-group-about-button").click(function() {
+	$("#specific-group-event-container").hide();
+	$("#specific-group-about-container").fadeIn();
+});
